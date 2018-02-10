bb1be8308cbe07210d431761d2f3907a2ecad1ce
Klin04 <kaijun_lin@yahoo.com>
2018-02-10 14:34:51 -0500

    finish webpage functionally, ready for backend dev

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,13 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
+import Signup from "./containers/Signup";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/login" exact component={Login} />
+    <Route patt="/signup" exact component={Signup}/>
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import { Link } from 'react-router-dom'
-import "./Login.css";
+import "./Signup.css";
 
-export default class Login extends Component {
+export default class Signup extends Component {
   constructor(props) {
     super(props);
 
@@ -13,8 +13,13 @@ export default class Login extends Component {
     };
   }
 
+  validatePassWord() {
+      return this.state.confirmpassword === this.state.password && this.state.password.length > 8
+  }
+
   validateForm() {
-    return this.state.email.length > 0 && this.state.password.length > 0;
+    return this.state.email.length > 0 && this.state.password.length > 0
+    && this.validatePassWord();
   }
 
   handleChange = event => {
@@ -27,10 +32,19 @@ export default class Login extends Component {
     event.preventDefault();
   }
 
+  handleSignUp = event => {
+      if (this.validateForm()) {
+          console.log("Sign up Successfully");
+          window.alert("Sign up Sucessfully");
+      } else {
+          window.alert("Please check your passwords");
+      }
+  }
+
 
   render() {
     return (
-      <div className="Login">
+      <div className="Signup">
         <form onSubmit={this.handleSubmit}>
           <FormGroup controlId="email" bsSize="large">
             <ControlLabel>Email</ControlLabel>
@@ -49,13 +63,22 @@ export default class Login extends Component {
               type="password"
             />
           </FormGroup>
+          <FormGroup controlId="confirmpassword" bsSize="large">
+            <ControlLabel>Confirm Password</ControlLabel>
+            <FormControl
+              value={this.state.confirmpassword}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
           <Button
             block
             bsSize="large"
             disabled={!this.validateForm()}
             type="submit"
+            onClick={(e) => this.handleSignUp(e)}
           >
-            Login
+            Sign Up
           </Button>
           <br></br>
           <Link to="/">
