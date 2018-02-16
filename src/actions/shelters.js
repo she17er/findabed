d66d9a1d1d3333edc81fff1a8018d8a57d5c4243
Klin04 <kaijun_lin@yahoo.com>
2018-02-16 13:05:08 -0500

    needs more work

--- src/actions/shelters.js
+++ src/actions/shelters.js
@@ -1,3 +1,12 @@
 import * as types from './types.js';
 import axios from 'axios';
 import { push } from 'react-router-redux';
+
+export function updateCurrentShelter(id) {
+    return dispatch => {
+        const body = {'currCapacity' : currCapacity};
+        axios.post('https://she17er.herokuapp.com/api/shelter/updateCapacity/:_id', body).then(resp => {
+            dispatch()
+        })
+
+}
