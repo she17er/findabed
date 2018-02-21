afead4ef7ec271fdb11f87df6be3e94edcb0dd15
krrishdholakia <krrishdholakia@gmail.com>
2018-02-20 19:18:22 -0500

    alamofire updates

---
+++ m4LoginReg/registrationSecondFile.swift
@@ -0,0 +1,80 @@
+//
+//  registrationSecondFile.swift
+//  m4LoginReg
+//
+//  Created by Krrish Dholakia on 2/14/18.
+//  Copyright © 2018 Krrish Dholakia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Alamofire
+
+
+class registrationSecondFile: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var ageTxtField: UITextField!
+    @IBOutlet weak var phoneTxtField: UITextField!
+    @IBOutlet weak var rolePickerView: UIPickerView!
+    @IBOutlet weak var emailTxtField: UITextField!
+    var roleTxt: String = ""
+    
+    var userName:String?
+    var password:String?
+    var gender:String?
+    var veteran:String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+            rolePickerView?.delegate = self
+            rolePickerView?.dataSource = self
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func onDoneClicked(_ sender: Any) {
+        let parameters: Parameters = [
+            "username": userName ?? "",
+            "age": ageTxtField.text ?? "",
+            "gender": gender ?? "",
+            "vet_S": veteran ?? "",
+            "contact": [
+                "phone": phoneTxtField.text,
+                "email": emailTxtField.text
+            ],
+            "password": password ?? "",
+            "role": roleTxt,
+            ]
+        
+        
+        Alamofire.request("https://she17er.herokuapp.com/api/users/newUsers", method: HTTPMethod.post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).validate().responseString {
+            (response) -> Void in
+            
+        }
+    }
+    
+    let roles = ["user", "admin", "shelter"]
+    
+    @available(iOS 2.0, *)
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    
+    // returns the # of rows in each component..
+    @available(iOS 2.0, *)
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return roles.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return roles[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        roleTxt = roles[row]
+    }
+    
+}
--- m4LoginReg/registrationViewController.swift
+++ m4LoginReg/registrationViewController.swift
@@ -8,81 +8,36 @@
 
 import Foundation
 import UIKit
-import Alamofire
-class registrationViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+class registrationViewController: UIViewController{
     
     @IBOutlet weak var userNameTxtField: UITextField!
     @IBOutlet weak var pwdTxtField: UITextField!
     @IBOutlet weak var genderTxtField: UITextField!
     @IBOutlet weak var veteranTxtField: UITextField!
-    @IBOutlet weak var phoneTxtField: UITextField!
-    @IBOutlet weak var ageTxtField: UITextField!
-    @IBOutlet weak var rolePickerView: UIPickerView!
-    @IBOutlet weak var emailTxtField: UITextField!
-    var roleTxt: String
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        rolePickerView.delegate = self
-        rolePickerView.dataSource = self
         
-        let parameters: Parameters = [
-            "username": userNameTxtField!,
-            "age": pwdTxtField!,
-            "gender": genderTxtField!,
-            "vet_S": veteranTxtField!,
-            "contact": [
-                "phone": phoneTxtField!,
-                "email": emailTxtField!
-            ],
-            "password": pwdTxtField!,
-            "role": roleTxt,
-        ]
-
-        var checker = false
-        Alamofire.request("https://she17er.herokuapp.com/api/users/newUsers", method: .post, parameters, encoding: JSONEncoding.default).validate().responseJSON {
-            (response) -> Void in
-            
-            if response.result.value != nil  {
-                checker = true
-            }
+        
+    }
+    
+    @IBAction func onNextClicked(_ sender: UIButton) {
+        if !userNameTxtField.text!.isEmpty &&
+            !pwdTxtField.text!.isEmpty &&
+            !genderTxtField.text!.isEmpty &&
+            !veteranTxtField.text!.isEmpty {
+            performSegue(withIdentifier: "registrationIdentifier", sender: self)
         }
-        
-        
     }
     
-    let roles = ["user", "admin", "shelter"]
-    
-    @available(iOS 2.0, *)
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1;
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? registrationSecondFile {
+            destination.userName = userNameTxtField.text
+            destination.password = pwdTxtField.text
+            destination.gender = genderTxtField.text
+            destination.veteran = veteranTxtField.text
+        }
     }
-    
-    
-    // returns the # of rows in each component..
-    @available(iOS 2.0, *)
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return roles.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return roles[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        roleTxt = roles[row]
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
-}
+
