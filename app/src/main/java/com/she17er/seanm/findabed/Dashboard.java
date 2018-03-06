3e5e9fbd72bfa1134416058b725d07b6c1610281
Elissa <elissa.huang1@gmail.com>
2018-03-06 00:48:36 -0500

    added csv file reader

--- app/src/main/java/com/she17er/seanm/findabed/Dashboard.java
+++ app/src/main/java/com/she17er/seanm/findabed/Dashboard.java
@@ -5,12 +5,19 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
  * Initial dashboard activity that a user sees upon login
  * Parses and displays all shelters from a CSV file as a list
+ *
+ * @edited by elissa huang
+ * @version 1.1
  */
 
 public class Dashboard extends AppCompatActivity {
@@ -25,7 +32,8 @@ public class Dashboard extends AppCompatActivity {
 
         // Initialize shelters
         shelters = new ArrayList<>();
-        addCSVShelters();
+        String csvFile = "C:/Users/eliss/Documents/GitHub/findabed/app/src/main/java/data/data.csv";
+        addCSVShelters(csvFile);
         // Create adapter passing in the sample user data
         ShelterAdapter adapter = new ShelterAdapter(this, shelters);
         // Attach the adapter to the recyclerview to populate items
@@ -34,9 +42,28 @@ public class Dashboard extends AppCompatActivity {
         shelterView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    public void addCSVShelters() {
-        shelters.add(new Shelter("Test Shelter 1"));
-        shelters.add(new Shelter("Test Shelter 2"));
-        shelters.add(new Shelter("Test Shelter 3"));
+    public void addCSVShelters(String filePath) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            String s;
+            while ((s = br.readLine()) != null) {
+                String[] tokens = s.split(",");
+                shelters.add(new Shelter(tokens));
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/she17er/seanm/findabed/Shelter.java
+++ app/src/main/java/com/she17er/seanm/findabed/Shelter.java
@@ -2,16 +2,107 @@ package com.she17er.seanm.findabed;
 
 /**
  * Represents a given shelter
+ *
+ * @author elissa huang
+ * @version 1.0
  */
 
 public class Shelter {
-    String name;
+    private String name;
+    private String capacity;
+    private String gender;
+    private double longitude;
+    private double latitude;
+    private String address;
+    private String phoneNumber;
 
-    public Shelter(String name) {
-        this.name = name;
+    public Shelter(String[] tokens) {
+        setName(tokens[1]);
+        setCapacity(tokens[2]);
+        setGender(tokens[3]);
+        setLongitude(tokens[4]);
+        setLatitude(tokens[5]);
+        setAddress(tokens[6]);
+        setPhoneNumber(tokens[8]);
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String s) {
+        if (s == null) {
+            this.capacity = "0 available";
+        } else {
+            this.capacity = s + " available";
+        }
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        if (gender.toLowerCase().contains("women")) {
+            if (gender.toLowerCase().contains("men")) {
+                this.gender = "all";
+            } else {
+                this.gender = "women";
+            }
+        } else if (gender.toLowerCase().contains("men")) {
+            this.gender = "men";
+        } else if (gender.toLowerCase().contains("any")){
+            this.gender = "all";
+        } else {
+            this.gender = "N/A";
+        }
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        try {
+            this.longitude = Double.valueOf(longitude);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        try {
+            this.latitude = Double.valueOf(latitude);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
