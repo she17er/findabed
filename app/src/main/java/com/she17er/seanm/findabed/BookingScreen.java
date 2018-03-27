3542814b7bf112ff4304e72cb2a286f2ab93a2fa
Sean Walsh <seanmichwalsh@protonmail.ch>
2018-03-27 10:59:47 -0400

    Setup booking screen

--- app/src/main/java/com/she17er/seanm/findabed/BookingScreen.java
+++ app/src/main/java/com/she17er/seanm/findabed/BookingScreen.java
@@ -1,15 +1,48 @@
 package com.she17er.seanm.findabed;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BookingScreen extends AppCompatActivity {
+
+    //UI Components
+    EditText numberText;
+    Button confirmButton;
+
+    //Shelter Data
+    int shelterPosition;
+    Shelter shelter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_screen);
-    }
 
-    
+        //Generates back button on action bar
+        if (getActionBar() != null) {
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        //Removes actionbar title
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        //Adds UI components
+        numberText = (EditText) findViewById(R.id.numberText);
+        confirmButton = (Button) findViewById(R.id.confirmButton);
+
+        //Initializes shelter data
+        Intent intent = this.getIntent();
+        if (intent.getExtras() != null) {
+            shelterPosition = Integer.parseInt(intent.getExtras().getString("shelterID"));
+            shelter = Dashboard.masterShelters.get(shelterPosition);
+        }
+        Log.d("bookingShelter", shelter.getName());
+    }
 }
