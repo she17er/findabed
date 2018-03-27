71d7a9132c1a3097ebcbe5181826d3c858fc5c91
Klin04 <kaijun_lin@yahoo.com>
2018-03-27 10:49:16 -0400

    added booking screen

---
+++ app/src/main/java/com/she17er/seanm/findabed/BookingScreen.java
@@ -0,0 +1,15 @@
+package com.she17er.seanm.findabed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking_screen);
+    }
+
+    
+}
