14742d1643dce18a6497f2cc2a56290fd163b06d
Klin04 <kaijun_lin@yahoo.com>
2018-03-28 16:31:51 -0400

    code working

--- app/src/main/java/com/she17er/seanm/findabed/SignupScreen.java
+++ app/src/main/java/com/she17er/seanm/findabed/SignupScreen.java
@@ -14,7 +14,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -209,10 +212,21 @@ public class SignupScreen extends AppCompatActivity {
 
                 Log.d("json", user.toString());
 
+
                 DataOutputStream localDataOutputStream = new DataOutputStream(connection.getOutputStream());
                 localDataOutputStream.writeBytes(user.toString());
                 localDataOutputStream.flush();
                 localDataOutputStream.close();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer content = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                Log.d("res", content.toString());
+                in.close();
+
 
             } catch (Exception e) {
                 Log.d("POSTError", e.toString());
