fd67ad9c936fa125140024c5dd20e5418b581837
Kevin Luikey <kluikey@gmail.com>
2018-04-16 11:53:50 -0400

    OnQueryTextChangeTester

--- app/src/androidTest/java/com/she17er/seanm/findabed/OnQueryTextChangeTester.java
+++ app/src/androidTest/java/com/she17er/seanm/findabed/OnQueryTextChangeTester.java
@@ -50,5 +50,149 @@ public class OnQueryTextChangeTester{
         textChangeTest.onQueryTextChange("abc");
         assertEquals(current, textChangeTest.shelterView);
     }
-    
+
+    @Test
+    @UiThreadTest
+    public void test2() {
+        Dashboard textChangeTest = rule.getActivity();
+
+        ArrayList<Shelter> correct = new ArrayList<Shelter>();
+
+        textChangeTest.currentShelters.clear();
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentShelters.get(0).setName("abcabcabc");
+        textChangeTest.currentShelters.get(1).setName("abca");
+
+        textChangeTest.populateShelterList(correct);
+        RecyclerView current = textChangeTest.shelterView;
+        textChangeTest.onQueryTextChange("ABC");
+        assertEquals(current, textChangeTest.shelterView);
+    }
+
+    @Test
+    @UiThreadTest
+    public void test3() {
+        Dashboard textChangeTest = rule.getActivity();
+
+        ArrayList<Shelter> correct = new ArrayList<Shelter>();
+        correct.add(new Shelter());
+        correct.add(new Shelter());
+        correct.get(0).setName("abCABcabc");
+        correct.get(1).setName("abCa");
+
+        textChangeTest.currentShelters.clear();
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentShelters.get(0).setName("abCABcabc");
+        textChangeTest.currentShelters.get(1).setName("abCa");
+
+        textChangeTest.populateShelterList(correct);
+        RecyclerView current = textChangeTest.shelterView;
+        textChangeTest.onQueryTextChange("abc");
+        assertEquals(current, textChangeTest.shelterView);
+    }
+
+    @Test
+    @UiThreadTest
+    public void test4() {
+        Dashboard textChangeTest = rule.getActivity();
+
+        ArrayList<Shelter> correct = new ArrayList<Shelter>();
+        correct.add(new Shelter());
+        correct.get(0).setName("abcadbcabc");
+
+        textChangeTest.currentShelters.clear();
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentShelters.get(0).setName("abcadbcabc");
+        textChangeTest.currentShelters.get(1).setName("abca");
+
+        textChangeTest.populateShelterList(correct);
+        RecyclerView current = textChangeTest.shelterView;
+        textChangeTest.onQueryTextChange("abcad");
+        assertEquals(current, textChangeTest.shelterView);
+    }
+
+    @Test
+    @UiThreadTest
+    public void test5() {
+        Dashboard textChangeTest = rule.getActivity();
+
+        ArrayList<Shelter> correct = new ArrayList<Shelter>();
+
+        textChangeTest.currentShelters.clear();
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentShelters.get(0).setName("abcabcabc");
+        textChangeTest.currentShelters.get(1).setName("abca");
+
+        textChangeTest.populateShelterList(correct);
+        RecyclerView current = textChangeTest.shelterView;
+        textChangeTest.onQueryTextChange("querty");
+        assertEquals(current, textChangeTest.shelterView);
+    }
+
+    @Test
+    @UiThreadTest
+    public void test6() {
+        Dashboard textChangeTest = rule.getActivity();
+
+        ArrayList<Shelter> correct = new ArrayList<Shelter>();
+
+        textChangeTest.currentShelters.clear();
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentShelters.get(0).setName("abcabcabc");
+        textChangeTest.currentShelters.get(1).setName("abca");
+
+        textChangeTest.populateShelterList(correct);
+        RecyclerView current = textChangeTest.shelterView;
+        textChangeTest.onQueryTextChange("QUERTY");
+        assertEquals(current, textChangeTest.shelterView);
+    }
+
+    @Test
+    @UiThreadTest
+    public void test7() {
+        Dashboard textChangeTest = rule.getActivity();
+
+        ArrayList<Shelter> correct = new ArrayList<Shelter>();
+        correct.add(new Shelter());
+        correct.get(0).setName("abcadbcabc");
+
+        textChangeTest.currentShelters.clear();
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentShelters.get(0).setName("abca");
+        textChangeTest.currentShelters.get(1).setName("abcadbcabc");
+
+        textChangeTest.populateShelterList(correct);
+        RecyclerView current = textChangeTest.shelterView;
+        textChangeTest.onQueryTextChange("abcad");
+        assertEquals(current, textChangeTest.shelterView);
+    }
+
+    @Test
+    @UiThreadTest
+    public void test8() {
+        Dashboard textChangeTest = rule.getActivity();
+
+        ArrayList<Shelter> correct = new ArrayList<Shelter>();
+        correct.add(new Shelter());
+        correct.add(new Shelter());
+        correct.get(0).setName("abca");
+        correct.get(1).setName("abcabcabc");
+
+        textChangeTest.currentShelters.clear();
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentSelters.add(new Shelter());
+        textChangeTest.currentShelters.get(0).setName("abca");
+        textChangeTest.currentShelters.get(1).setName("abcabcabc");
+
+        textChangeTest.populateShelterList(correct);
+        RecyclerView current = textChangeTest.shelterView;
+        textChangeTest.onQueryTextChange("abc");
+        assertEquals(current, textChangeTest.shelterView);
+    }
 }
