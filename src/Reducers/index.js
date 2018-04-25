9cecbc4a31a48b63751f773232096a1a55da1491
Sean Walsh <seanmichwalsh@protonmail.ch>
2018-04-24 23:31:31 -0400

    Removed redundant duplicate files

--- src/Reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer } from 'react-redux-sweetalert';
-import { routerReducer as router } from 'react-router-redux';
-import Auth from './Auth'
-export default combineReducers({
-    Auth,
-});
